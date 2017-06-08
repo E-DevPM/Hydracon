1989934e97becd481f0877c567c78729a99e47af
CookieCode <etienne.crosnier.la@gmail.com>
2017-06-09 00:40:43 +0200

    Update ??

--- src/pocketmine/network/protocol/LoginPacket.php
+++ src/pocketmine/network/protocol/LoginPacket.php
@@ -37,42 +37,61 @@ class LoginPacket extends DataPacket{
 	public $identityPublicKey;
 	public $serverAddress;
 
-	public $skinId;
+	public $skinId = null;
 	public $skin = null;
 
 	public $clientData = [];
 
+	public $deviceModel;
+	public $os;
+
 	public function decode(){
 		$this->protocol = $this->getInt();
-
-		if($this->protocol !== Info::CURRENT_PROTOCOL){
+		if(!in_array($this->protocol, Info::ACCEPTED_PROTOCOLS)){
 			$this->buffer = null;
-			return; //Do not attempt to decode for non-accepted protocols
+			return;
 		}
 
 		$this->gameEdition = $this->getByte();
 
-		$str = zlib_decode($this->getString(), 1024 * 1024 * 64);
+		$this->setBuffer($this->getString(), 0);
 
-		$this->setBuffer($str, 0);
+		$time = time();
 
-		$chainData = json_decode($this->get($this->getLInt()));
-		foreach($chainData->{"chain"} as $chain){
-			$webtoken = $this->decodeToken($chain);
-			if(isset($webtoken["extraData"])){
-				if(isset($webtoken["extraData"]["displayName"])){
-					$this->username = $webtoken["extraData"]["displayName"];
+		$chainData = json_decode($this->get($this->getLInt()))->{"chain"};
+		// Start with the trusted one
+		$chainKey = self::MOJANG_PUBKEY;
+		while(!empty($chainData)){
+			foreach($chainData as $index => $chain){
+				list($verified, $webtoken) = $this->decodeToken($chain, $chainKey);
+				if(isset($webtoken["extraData"])){
+					if(isset($webtoken["extraData"]["displayName"])){
+						$this->username = $webtoken["extraData"]["displayName"];
+					}
+					if(isset($webtoken["extraData"]["identity"])){
+						$this->clientUUID = $webtoken["extraData"]["identity"];
+					}
 				}
-				if(isset($webtoken["extraData"]["identity"])){
-					$this->clientUUID = $webtoken["extraData"]["identity"];
+				if($verified){
+					$verified = isset($webtoken["nbf"]) && $webtoken["nbf"] <= $time && isset($webtoken["exp"]) && $webtoken["exp"] > $time;
 				}
-				if(isset($webtoken["identityPublicKey"])){
-					$this->identityPublicKey = $webtoken["identityPublicKey"];
+				if($verified and isset($webtoken["identityPublicKey"])){
+					// Looped key chain. #blamemojang
+					if($webtoken["identityPublicKey"] != self::MOJANG_PUBKEY) $chainKey = $webtoken["identityPublicKey"];
+					break;
+				}elseif($chainKey === null){
+					// We have already gave up
+					break;
 				}
+			}
+			if(!$verified && $chainKey !== null){
+				$chainKey = null;
+			}else{
+				unset($chainData[$index]);
 			}
 		}
 
-		$this->clientData = $this->decodeToken($this->get($this->getLInt()));
+		list($verified, $this->clientData) = $this->decodeToken($this->get($this->getLInt()), $chainKey);
 
 		$this->clientId = $this->clientData["ClientRandomId"] ?? null;
 		$this->serverAddress = $this->clientData["ServerAddress"] ?? null;
@@ -81,16 +100,53 @@ class LoginPacket extends DataPacket{
 		if(isset($this->clientData["SkinData"])){
 			$this->skin = base64_decode($this->clientData["SkinData"]);
 		}
+
+		if(isset($this->clientData["DeviceModel"])){
+			$this->deviceModel = $this->clientData["DeviceModel"];
+		}
+
+		if(isset($this->clientData["DeviceOS"])){
+			$this->os = $this->clientData["DeviceOS"];
+		}
+
+		if($verified){
+			$this->identityPublicKey = $chainKey;
+		}
 	}
 
 	public function encode(){
 
 	}
 
-	public function decodeToken($token){
+	public function decodeToken($token, $key){
 		$tokens = explode(".", $token);
 		list($headB64, $payloadB64, $sigB64) = $tokens;
 
-		return json_decode(base64_decode($payloadB64), true);
+		if($key !== null and extension_loaded("openssl")){
+			$sig = base64_decode(strtr($sigB64, '-_', '+/'), true);
+			$rawLen = 48; // ES384
+			for($i = $rawLen; $i > 0 and $sig[$rawLen - $i] == chr(0); $i--) {}
+			$j = $i + (ord($sig[$rawLen - $i]) >= 128 ? 1 : 0);
+			for($k = $rawLen; $k > 0 and $sig[2 * $rawLen - $k] == chr(0); $k--) {}
+			$l = $k + (ord($sig[2 * $rawLen - $k]) >= 128 ? 1 : 0);
+			$len = 2 + $j + 2 + $l;
+			$derSig = chr(48);
+			if($len > 255){
+				throw new \RuntimeException("Invalid signature format");
+			}elseif($len >= 128){
+				$derSig .= chr(81);
+			}
+			$derSig .= chr($len) . chr(2) . chr($j);
+			$derSig .= str_repeat(chr(0), $j - $i) . substr($sig, $rawLen - $i, $i);
+			$derSig .= chr(2) . chr($l);
+			$derSig .= str_repeat(chr(0), $l - $k) . substr($sig, 2 * $rawLen - $k, $k);
+
+			$verified = openssl_verify($headB64 . "." . $payloadB64, $derSig, "-----BEGIN PUBLIC KEY-----\n" . wordwrap($key, 64, "\n", true) . "\n-----END PUBLIC KEY-----\n", OPENSSL_ALGO_SHA384) === 1;
+		}else{
+			$verified = false;
+		}
+
+		return array($verified, json_decode(base64_decode($payloadB64), true));
 	}
+
 }
