ddd85e0556d687540b32c8bf4fc9068a370944e6
CookieCode <etienne.crosnier.la@gmail.com>
2017-06-08 15:01:11 +0200

    Add start.sh/cmd

---
+++ start.cmd
@@ -0,0 +1,28 @@
+@echo off
+TITLE PocketMine-MP server software for Minecraft: Pocket Edition
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist PocketMine-MP.phar (
+	set POCKETMINE_FILE=Hydracon.phar
+) else (
+	if exist src\pocketmine\PocketMine.php (
+		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	) else (
+		echo "Couldn't find a valid Hydracon installation"
+		pause
+		exit 1
+	)
+)
+
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Hydracon" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
