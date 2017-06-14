0e82d45dd7d151ab66e81e4e84741f4fc8b77273
Az928 <TheAz928@users.noreply.github.com>
2017-06-14 16:26:13 +0600

    Fix namespace error (1/2)

--- src/pocketmine/level/sound/NoteblockSound.php
+++ src/pocketmine/level/sound/NoteblockSound.php
@@ -22,8 +22,8 @@
 namespace pocketmine\level\sound;
 
 use pocketmine\math\Vector3;
-use pocketmine\network\protocol\BlockEventPacket;
-use pocketmine\network\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\BlockEventPacket;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
 
 class NoteblockSound extends GenericSound{
 	protected $instrument;
